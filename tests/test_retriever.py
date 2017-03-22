dddb3d2a7fd3a4f6e097d4cb90da623eb33752b8
Tejas Patel <tejas.patel@ons.gov.uk>
2017-03-22 09:45:01 +0000

    Unit test fixes

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -1,10 +1,8 @@
 import sys
 sys.path.append('../ras-secure-message')
-from app import application
 import unittest
 from flask import json
 from sqlalchemy import create_engine
-from app import settings
 from app.repository.retriever import Retriever
 from app.application import app
 from flask import current_app
@@ -15,10 +13,11 @@ class FlaskTestCase(unittest.TestCase):
 
     def setUp(self):
         app.testing = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = settings.SECURE_MESSAGING_DATABASE_URL
-        self.engine = create_engine(settings.SECURE_MESSAGING_DATABASE_URL, echo=True)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/messages.db'
+        self.engine = create_engine('sqlite:////tmp/messages.db', echo=True)
         with app.app_context():
             database.db.init_app(current_app)
+            database.db.drop_all()
             database.db.create_all()
             self.db = database.db
         pass
@@ -31,17 +30,12 @@ class FlaskTestCase(unittest.TestCase):
         pass
 
     def test_0_msg_returned_when_db_empty_true(self):
-        with self.engine.connect() as con:
-            con.execute('DELETE FROM secure_message')
         with app.app_context():
             with current_app.test_request_context():
                 response = Retriever().retrieve_message_list()
                 self.assertEqual(json.loads(response.get_data()), [])
 
     def test_all_msg_returned_when_db_less_than_15(self):
-        with self.engine.connect() as con:
-            con.execute('DELETE FROM secure_message')
-
         self.populate_database(5)
 
         with app.app_context():
@@ -50,9 +44,6 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(len(json.loads(response.get_data())), 5)
 
     def test_15_msg_returned_when_db_greater_than_15(self):
-        with self.engine.connect() as con:
-            con.execute('DELETE FROM secure_message')
-
         self.populate_database(20)
         with app.app_context():
             with current_app.test_request_context():
