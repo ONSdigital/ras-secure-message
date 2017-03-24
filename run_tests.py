57bd9722d70efeb3639fd582bad79b3b2a7bac5e
Richard Weeks <richard.weeks@ons.gov.uk>
2017-03-24 14:53:01 +0000

    Return exit code for script.

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,14 @@
 import unittest
-
+import os
+import sys
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().discover('./tests/app')
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
+    test_dirs = os.listdir('./tests')
+    suites_list = []
+    loader = unittest.TestLoader()
+    for directory in test_dirs:
+        test_path = "./tests/{}".format(directory)
+        suite = loader.discover(test_path)
+        suites_list.append(suite)
+        result = unittest.TextTestRunner(verbosity=2).run(suite)
+        sys.exit(result.failures)
