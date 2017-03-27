be9453c416c5d8c2865427f386e9732dae3dbd3b
Tejas Patel <tejas.patel@ons.gov.uk>
2017-03-27 08:37:30 +0100

    Travis build fix

--- run_tests.py
+++ run_tests.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
         suite = loader.discover(test_path)
         suites_list.append(suite)
         result = unittest.TextTestRunner(verbosity=2).run(suite)
-        sys.exit(result.failures)
+        if len(result.failures) != 0:
+            sys.exit(1)
