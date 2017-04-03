85aeb8d7bf9af380e2c8e037b7fcd4518dfe7285
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2017-04-03 13:56:56 +0100

    TS-142 Missing read , archive and thread id were not working externally

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -139,5 +139,72 @@ class FlaskTestCase(unittest.TestCase):
         self.assertTrue(len(original_thread_id) == 36)  # UUID length
         self.assertTrue(len(reply_msg_id) == 36)
 
+    def test_missing_read_status_does_not_cause_exception(self):
+        url = "http://localhost:5050/message/send"
+        headers = {'Content-Type': 'application/json'}
+        now = datetime.now(timezone.utc)
+        test_message = {'msg_id': 'AMsgId',    # No read_Status
+                        'msg_to': 'richard',
+                        'msg_from': 'torrance',
+                        'subject': 'MyMessage',
+                        'body': 'hello',
+                        'thread_id': "",
+                        'archive_status': False,
+                        'sent_date': now,
+                        'read_date': now,
+                        'collection_case': 'ACollectionCase',
+                        'reporting_unit': 'AReportingUnit',
+                        'collection_instrument': 'ACollectionInstrument'}
+        try:
+            self.app.post(url, data=json.dumps(test_message), headers=headers)
+            self.assertTrue(True)  # i.e no exception
+        except Exception as e:
+            self.fail( "post rasied unexpected exception: {0}".format(e))
+
+    def test_missing_archive_status_does_not_cause_exception(self):
+        url = "http://localhost:5050/message/send"
+        headers = {'Content-Type': 'application/json'}
+        now = datetime.now(timezone.utc)
+        test_message = {'msg_id': 'AMsgId',    # No read_Status
+                        'msg_to': 'richard',
+                        'msg_from': 'torrance',
+                        'subject': 'MyMessage',
+                        'body': 'hello',
+                        'thread_id': "",
+                        'read_status': False,
+                        'sent_date': now,
+                        'read_date': now,
+                        'collection_case': 'ACollectionCase',
+                        'reporting_unit': 'AReportingUnit',
+                        'collection_instrument': 'ACollectionInstrument'}
+        try:
+            self.app.post(url, data=json.dumps(test_message), headers=headers)
+            self.assertTrue(True)  # i.e no exception
+        except Exception as e:
+            self.fail( "post raised unexpected exception: {0}".format(e))
+
+    def test_missing_thread_id_does_not_cause_exception(self):
+        url = "http://localhost:5050/message/send"
+        headers = {'Content-Type': 'application/json'}
+        now = datetime.now(timezone.utc)
+        test_message = {'msg_id': 'AMsgId',  # No read_Status
+                        'msg_to': 'richard',
+                        'msg_from': 'torrance',
+                        'subject': 'MyMessage',
+                        'body': 'hello',
+                        'read_status': False,
+                        'archive_status': False,
+                        'sent_date': now,
+                        'read_date': now,
+                        'collection_case': 'ACollectionCase',
+                        'reporting_unit': 'AReportingUnit',
+                        'collection_instrument': 'ACollectionInstrument'}
+        try:
+            self.app.post(url, data=json.dumps(test_message), headers=headers)
+            self.assertTrue(True)  # i.e no exception
+        except Exception as e:
+            self.fail("post raised unexpected exception: {0}".format(e))
+
+
 if __name__ == '__main__':
     unittest.main()
