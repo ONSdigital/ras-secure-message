03907c4cf6507ae850f38456c2da03d45d64d3e6
Tejas Patel <tejas.patel@ons.gov.uk>
2017-04-07 13:25:27 +0100

    Database changes

--- app/repository/database.py
+++ app/repository/database.py
@@ -16,8 +16,6 @@ class SecureMessage(db.Model):
 
     id = Column("id", Integer, primary_key=True)
     msg_id = Column("msg_id", String(constants.MAX_MSG_ID_LEN), unique=True)
-    msg_to = Column("msg_to", String(constants.MAX_TO_LEN+1))
-    msg_from = Column("msg_from", String(constants.MAX_FROM_LEN+1))
     subject = Column("subject", String(constants.MAX_SUBJECT_LEN+1))
     body = Column("body", String(constants.MAX_BODY_LEN+1))
     thread_id = Column("thread_id", String(constants.MAX_THREAD_LEN + 1))
@@ -27,13 +25,11 @@ class SecureMessage(db.Model):
     reporting_unit = Column("reporting_unit", String(constants.MAX_REPORTING_UNIT_LEN+1))
     collection_instrument = Column("collection_instrument", String(constants.MAX_COLLECTION_INSTRUMENT_LEN+1))
 
-    def __init__(self, msg_id="", msg_to="", msg_from="", subject="", body="", thread_id="",
+    def __init__(self, msg_id="", subject="", body="", thread_id="",
                  sent_date=datetime.now(timezone.utc), read_date=None, collection_case='',
-                 reporting_unit='', collection_instrument=''):
+                 reporting_unit='', survey=''):
         logger.debug("Initialised Secure Message entity: msg_id: {}".format(id))
         self.msg_id = msg_id
-        self.msg_to = msg_to
-        self.msg_from = msg_from
         self.subject = subject
         self.body = body
         self.thread_id = thread_id
@@ -41,13 +37,11 @@ class SecureMessage(db.Model):
         self.read_date = read_date
         self.collection_case = collection_case
         self.reporting_unit = reporting_unit
-        self.collection_instrument = collection_instrument
+        self.survey = survey
 
     def set_from_domain_model(self, domain_model):
         """set dbMessage attributes to domain_model attributes"""
         self.msg_id = domain_model.msg_id
-        self.msg_to = domain_model.msg_to
-        self.msg_from = domain_model.msg_from
         self.subject = domain_model.subject
         self.body = domain_model.body
         self.thread_id = domain_model.thread_id
@@ -55,16 +49,13 @@ class SecureMessage(db.Model):
         self.read_date = domain_model.read_date
         self.collection_case = domain_model.collection_case
         self.reporting_unit = domain_model.reporting_unit
-        self.collection_instrument = domain_model.reporting_unit
+        self.survey = domain_model.survey
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         data = {
-            'id': self.id,
             'msg_id': self.msg_id,
-            'msg_to': self.msg_to,
-            'msg_from': self.msg_from,
             'subject': self.subject,
             'body': self.body,
             'thread_id': self.thread_id,
@@ -72,7 +63,7 @@ class SecureMessage(db.Model):
             'read_date': self.read_date,
             'collection_case': self.collection_case,
             'reporting_unit': self.reporting_unit,
-            'collection_instrument': self.collection_instrument,
+            'survey': self.survey,
             '_links': ''
         }
         return data
@@ -101,9 +92,34 @@ class Status(db.Model):
     def serialize(self):
         """Return object data in easily serializeable format"""
         data = {
-            'id': self.id,
             'label': self.label,
             'msg_id': self.msg_id,
             'actor': self.actor
+        }
+        return data
+
+
+class InternalSentAudit(db.Model):
+    """Label Assignment table model"""
+    __tablename__ = "internal_sent_audit"
+
+    id = Column("id", Integer, primary_key=True)
+    msg_id = Column('msg_id', String(constants.MAX_MSG_ID_LEN + 1), ForeignKey('secure_message.msg_id'))
+    internal_user = Column('internal_user', String(constants.MAX_STATUS_ACTOR_LEN + 1))
+
+    def __init__(self, msg_id='', internal_user=''):
+        self.msg_id = msg_id
+        self.internal_urn = internal_user
+
+    def set_from_domain_model(self, domain_model):
+        self.msg_id = domain_model.msg_id
+        self.internal_user = domain_model.internal_user
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        data = {
+            'msg_id': self.msg_id,
+            'internal_user': self.internal_user
         }
         return data
