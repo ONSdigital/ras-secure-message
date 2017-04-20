72ac68088325dc2bca47fb026ce4f7b44c9d8c81
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2017-04-20 14:06:00 +0100

    Added new unit tests for Saver to cover throwing of exceptions

--- app/repository/saver.py
+++ app/repository/saver.py
@@ -12,46 +12,40 @@ class Saver:
     """Created when saving a message"""
 
     @staticmethod
-    def save_message(domain_message):
+    def save_message(domain_message, session=db.session):
         """save message to database"""
 
         db_message = database.SecureMessage()
         domain_message.sent_date = datetime.now(timezone.utc)  # LOGIC NEEDED: set sent_date only for sent message
         db_message.set_from_domain_model(domain_message)
         try:
-            db.session.add(db_message)
-            db.session.commit()
+            session.add(db_message)
+            session.commit()
         except Exception as e:
-            logger.error("Message save failed {}".format(e))
+            logger.error("Message save failed {0}".format(e))
             raise MessageSaveException(e)
 
     @staticmethod
-    def convert_to_datamodel(domain_message):
-        return database.SecureMessage(domain_message.msg_to, domain_message.msg_from, domain_message.subject,
-                                      domain_message.body, domain_message.thread_id, domain_message.sent_date,
-                                      domain_message.read_date, domain_message.msg_id)
-
-    @staticmethod
-    def save_msg_status(msg_urn, msg_id, label):
+    def save_msg_status(msg_urn, msg_id, label, session=db.session):
         """save message status to database"""
 
         db_status_to = database.Status()
         db_status_to.set_from_domain_model(msg_id, msg_urn, label)
         try:
-            db.session.add(db_status_to)
-            db.session.commit()
+            session.add(db_status_to)
+            session.commit()
         except Exception as e:
             logger.error("Message status save failed {}".format(e))
             raise MessageSaveException(e)
 
     @staticmethod
-    def save_msg_audit(msg_id, msg_urn):
+    def save_msg_audit(msg_id, msg_urn, session=db.session):
         """Save Sent Audit data to database"""
         db_audit = database.InternalSentAudit()
         db_audit.set_from_domain_model(msg_id, msg_urn)
         try:
-            db.session.add(db_audit)
-            db.session.commit()
+            session.add(db_audit)
+            session.commit()
         except Exception as e:
             logger.error("Message audit save failed {}".format(e))
             raise MessageSaveException(e)
--- tests/app/test_saver.py
+++ tests/app/test_saver.py
@@ -2,8 +2,11 @@ import unittest
 from app.repository.saver import Saver
 from sqlalchemy import create_engine
 from app.repository import database
+from app.repository.database import db
 from flask import current_app
 from app.application import app
+from unittest import mock
+from app.exception.exceptions import MessageSaveException
 
 from app.validation.domain import Message
 
@@ -15,6 +18,8 @@ class SaverTestCase(unittest.TestCase):
         app.testing = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/messages.db'
         self.engine = create_engine('sqlite:////tmp/messages.db', echo=True)
+        self.test_message = Message(**{'urn_to': 'tej', 'urn_from': 'gemma', 'subject': 'MyMessage',
+                                    'body': 'hello', 'thread_id': ""})
         with app.app_context():
             database.db.init_app(current_app)
             database.db.drop_all()
@@ -23,17 +28,26 @@ class SaverTestCase(unittest.TestCase):
 
     def test_saved_message_has_saved_sent_date(self):
         """retrieves messages from database with less entries than retrieval amount"""
-        message_object = Message(**{'urn_to': 'tej', 'urn_from': 'gemma', 'subject': 'MyMessage',
-                                    'body': 'hello', 'thread_id': ""})
+
         with app.app_context():
             with current_app.test_request_context():
-                Saver().save_message(message_object)
+                Saver().save_message(self.test_message)
 
         with self.engine.connect() as con:
             request = con.execute('SELECT * FROM secure_message')
             for row in request:
                 data = {"sent_date": row['sent_date']}
                 self.assertTrue(data['sent_date'] is not None)
+
+    def test_save_message_rasies_MessageSaveException_on_db_error(self):
+        """Tests exception is logged if message save fails"""
+
+        mock_session = mock.Mock(db.session)
+        mock_session.commit.side_effect = Exception("Not Saved")
+        with app.app_context():
+            with current_app.test_request_context():
+                with self.assertRaises(MessageSaveException):
+                    Saver().save_message(self.test_message, mock_session)
 
     def test_saved_msg_status_has_been_saved(self):
         """retrieves message status from database"""
@@ -47,8 +61,17 @@ class SaverTestCase(unittest.TestCase):
             for row in request:
                 self.assertTrue(row is not None)
 
+    def test_save_msg_status_raises_MessageSaveException_on_db_error(self):
+        mock_session = mock.Mock(db.session)
+        mock_session.commit.side_effect = Exception("Not Saved")
+        message_status = {'msg_id': 'AMsgId', 'actor': 'Tej'}
+        with app.app_context():
+            with current_app.test_request_context():
+                with self.assertRaises(MessageSaveException):
+                    Saver().save_msg_status(message_status['msg_id'], message_status['actor'], 'INBOX', mock_session)
+
     def test_msg_audit_has_been_saved(self):
-        """Tests message audif is saved to database"""
+        """Tests message audit is saved to database"""
         messageaudit_object = {'msg_id': 'MsgId', 'msg_urn': 'Tej'}
         with app.app_context():
             with current_app.test_request_context():
@@ -58,3 +81,12 @@ class SaverTestCase(unittest.TestCase):
             request = con.execute('SELECT * FROM internal_sent_audit')
             for row in request:
                 self.assertTrue(row is not None)
+
+    def test_save_msg_audit_raises_MessageSaveException_on_db_error(self):
+        mock_session = mock.Mock(db.session)
+        mock_session.commit.side_effect = Exception("Not Saved")
+        messageaudit_object = {'msg_id': 'MsgId', 'msg_urn': 'Tej'}
+        with app.app_context():
+            with current_app.test_request_context():
+                with self.assertRaises(MessageSaveException):
+                    Saver().save_msg_audit(messageaudit_object['msg_id'], messageaudit_object['msg_urn'],mock_session)
