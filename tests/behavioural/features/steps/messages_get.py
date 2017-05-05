92558d4eb6134e7fcfcdcf4da3183da7c09a79a3
Gemma Irving <gemma.irving@ons.gov.uk>
2017-05-05 12:38:56 +0100

    Implement new BDD test get list of messages

--- tests/behavioural/features/steps/messages_get.py
+++ tests/behavioural/features/steps/messages_get.py
@@ -9,8 +9,7 @@ from flask import current_app
 url = "http://localhost:5050/messages"
 headers = {'Content-Type': 'application/json', 'user_urn': ''}
 
-data = {'msg_id': '',
-        'urn_to': 'test',
+data = {'urn_to': 'test',
         'urn_from': 'test',
         'subject': 'Hello World',
         'body': 'Test',
@@ -33,7 +32,6 @@ def reset_db():
 def step_impl(context):
     reset_db()
     for x in range(0, 2):
-        data['msg_id'] = str(uuid.uuid4())
         data['urn_to'] = 'internal.12344'
         data['urn_from'] = 'respondent.122342'
         context.response = app.test_client().post("http://localhost:5050/message/send",
@@ -60,7 +58,6 @@ def step_impl(context):
 def step_impl(context):
     reset_db()
     for x in range(0, 2):
-        data['msg_id'] = str(uuid.uuid4())
         data['urn_to'] = 'respondent.122342'
         data['urn_from'] = 'internal.12344'
         context.response = app.test_client().post("http://localhost:5050/message/send",
@@ -85,3 +82,24 @@ def step_impl(context):
         nose.tools.assert_true(len(response['messages'][str(num)]['labels']), 2)
         nose.tools.assert_true('INBOX' in response['messages'][str(num)]['labels'])
         nose.tools.assert_true('UNREAD' in response['messages'][str(num)]['labels'])
+
+# Scenario: As an external user I would like to be able to view a lst of messages
+
+
+@given("multiple messages have been sent to an external user")
+def step_impl(context):
+    for x in range(0, 2):
+        data['urn_to'] = 'respondent.123'
+        app.test_client().post("http://localhost:5050/message/send", headers=headers)
+
+
+@when("the external user navigates to their messages")
+def step_impl(context):
+    headers['user_urn'] = 'respondent.123'
+    context.response = app.test_client().get(url, headers=headers)
+
+
+@then("messages are displayed")
+def step_impl(context):
+    response = flask.json.loads(context.response.data)
+    nose.tools.assert_true(len(response['messages']), 2)
