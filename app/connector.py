d9b4fd8d183efdae495c0a8639e0d8e848f42952
Richard Weeks <richard.weeks@live.co.uk>
2017-05-12 14:13:11 +0100

    Configuration for Cloud Foundry. (#5)
    * Configuration for Cloud Foundry.
    
    * Configure dev port.

---
+++ app/connector.py
@@ -0,0 +1,34 @@
+import os
+import json
+import logging
+
+conn = None
+# uri = 'postgres://postgres:password@host.pcfdev.io:5431/postgres'
+uri = None
+
+### Extract the database URI value from VCAP_SERVICES
+def getDatabaseUri():
+
+    logger = logging.getLogger(__name__)
+    global uri
+
+    if uri is not None:
+        return uri
+
+    if 'VCAP_SERVICES' in os.environ:
+        logger.info('VCAP_SERVICES found in environment')
+        decoded_config = json.loads(os.environ['VCAP_SERVICES'])
+    else:
+        logger.info('VCAP_SERVICES NOT found in environment')
+        return os.environ.get('SECURE_MESSAGING_DATABASE_URL', 'sqlite:////tmp/messages.db')
+
+    for key, value in decoded_config.items():
+        logger.info('Inspecting key: "' + str(key) + '" with value: ' + str(value))
+        if decoded_config[key][0]['name'] == 'db_ras_sm':
+            creds = decoded_config[key][0]['credentials']
+            uri = creds['uri']
+            logger.info('Postgres DATABASE URI: ' + uri)
+            return uri
+        else:
+            logger.info('VCAP_SERVICES defined but no URI credential found')
+            return os.environ.get('SECURE_MESSAGING_DATABASE_URL', 'sqlite:////tmp/messages.db')
