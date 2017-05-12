0cb397d7372ea08331280ca0a86edc6b90611c70
Richard Weeks <richard.weeks@ons.gov.uk>
2017-05-12 14:07:24 +0100

    Configure dev port.

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!flask/bin/python
 import logging
+import os
 from app.application import app
-app.run(debug=True, host='0.0.0.0', port=8080)
+
+DEV_PORT = os.getenv('DEV_PORT', 5050)
+app.run(debug=True, host='0.0.0.0', port=DEV_PORT)
 
 app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
