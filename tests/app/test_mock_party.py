c1e64a8c853b9496472eb47ebdddd49898eed76c
Gemma Irving <gemma.irving@ons.gov.uk>
2017-06-19 11:14:26 +0100

    Add tests

--- tests/app/test_mock_party.py
+++ tests/app/test_mock_party.py
@@ -11,7 +11,7 @@ class PartyTestCase(unittest.TestCase):
 
         user_details = user_by_uuid.get_details_by_uuid(list_uuids)
 
-        self.assertTrue(user_details is not None)
+        self.assertEqual(user_details[list_uuids[0]], '{"firstname": "Bhavana", "surname": "Lincoln", "telephone": "+443069990888", "status": "ACTIVE"}')
 
     def test_get_user_details_by_uuids(self):
         """Test that user details are returned using uuids"""
@@ -20,7 +20,9 @@ class PartyTestCase(unittest.TestCase):
 
         user_details = user_by_uuid.get_details_by_uuid(list_uuids)
 
-        self.assertTrue(user_details is not None)
+        self.assertEqual(user_details[list_uuids[0]], '{"firstname": "Bhavana", "surname": "Lincoln", "telephone": "+443069990888", "status": "ACTIVE"}')
+        self.assertEqual(user_details[list_uuids[1]], '{"firstname": "Chandana", "surname": "Blanchet", "telephone": "+443069990854", "status": "ACTIVE"}')
+        self.assertEqual(user_details[list_uuids[2]], '{"firstname": "Ida", "surname", "Larue", "telephone": "+443069990250", "status": "ACTIVE"}')
 
 if __name__ == '__main__':
     unittest.main()
