d9fbfc22072af6c39f9080348c939cc3d1f82134
Tejas Patel <tejas.patel@ons.gov.uk>
2017-06-23 09:51:16 +0100

    Changed format of msg_to validation

--- app/validation/domain.py
+++ app/validation/domain.py
@@ -57,10 +57,12 @@ class MessageSchema(Schema):
 
     @pre_load
     def check_format_of_msg_to_and_msg_from(self, data):
-        if data.get('msg_to') and not isinstance(data.get('msg_to'), str) and "id" in data.get('msg_to'):
+        if data.get('msg_to') and type(data.get('msg_to')) is list and "id" in data.get('msg_to')[0]:
             data['msg_to'] = data['msg_to']['id']
-        elif data.get('msg_to') and not isinstance(data.get('msg_to'), str):
-            raise ValidationError("'msg_to' is missing an 'id'")
+        elif data.get('msg_to') and type(data.get('msg_to')) is list:
+            raise ValidationError("'msg_to' is missing an 'id' or incorrect format")
+        elif not data.get('msg_to') and type(data.get('msg_to')) is list:
+            data['msg_to'] = ''
 
         if data.get('msg_from') and not isinstance(data.get('msg_from'), str) and "id" in data.get('msg_from'):
             data['msg_from'] = data['msg_from']['id']
@@ -154,10 +156,12 @@ class DraftSchema(Schema):
 
     @pre_load
     def check_format_of_msg_to_and_msg_from(self, data):
-        if data.get('msg_to') and not isinstance(data.get('msg_to'), str) and "id" in data.get('msg_to'):
+        if data.get('msg_to') and type(data.get('msg_to')) is list and "id" in data.get('msg_to')[0]:
             data['msg_to'] = data['msg_to']['id']
-        elif data.get('msg_to') and not isinstance(data.get('msg_to'), str):
-            raise ValidationError("'msg_to' is missing an 'id'")
+        elif data.get('msg_to') and type(data.get('msg_to')) is list:
+            raise ValidationError("'msg_to' is missing an 'id' or incorrect format")
+        elif not data.get('msg_to') and type(data.get('msg_to')) is list:
+            data['msg_to'] = ''
 
         if data.get('msg_from') and not isinstance(data.get('msg_from'), str) and "id" in data.get('msg_from'):
             data['msg_from'] = data['msg_from']['id']
