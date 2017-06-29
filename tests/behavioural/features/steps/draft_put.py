dea924a897751bb51991976d95a7bdca4a773592
Tejas Patel <tejas.patel@ons.gov.uk>
2017-06-29 14:05:49 +0100

    Validating user and refactoring

--- tests/behavioural/features/steps/draft_put.py
+++ tests/behavioural/features/steps/draft_put.py
@@ -9,8 +9,8 @@ from app import settings, constants
 
 url = "http://localhost:5050/draft/{0}/modify"
 token_data = {
-            "user_uuid": "ce12b958-2a5f-44f4-a6da-861e59070a31",
-            "role": "internal"
+            "user_uuid": "0a7ad740-10d5-4ecb-b7ca-3c0384afb882",
+            "role": "respondent"
         }
 headers = {'Content-Type': 'application/json', 'Authorization': ''}
 
@@ -48,19 +48,42 @@ def update_encrypted_jwt():
     signed_jwt = encode(token_data)
     return encrypter.encrypt_token(signed_jwt)
 
-headers['Authorization'] = update_encrypted_jwt()
+
+def before_scenario(context):
+    headers['Authorization'] = update_encrypted_jwt()
+
+    post_data.update({'msg_to': 'BRES',
+                 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
+                 'subject': 'test',
+                 'body': 'Test',
+                 'thread_id': '2',
+                 'collection_case': 'collection case1',
+                 'collection_exercise': 'collection exercise1',
+                 'ru_ref': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
+                 'survey': 'BRES'})
+
+    data.update({'msg_to': 'BRES',
+            'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
+            'subject': 'test',
+            'body': 'Test',
+            'thread_id': '2',
+            'collection_case': 'collection case1',
+            'collection_exercise': 'collection exercise1',
+            'ru_ref': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
+            'survey': 'BRES'})
 
 
 # Scenario 1: A user edits a previously saved draft
 @given('a user edits a previously saved draft')
 def step_impl_user_edits_saved_draft(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
     context.etag = get_draft.headers.get('ETag')
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -116,7 +139,7 @@ def step_impl_success_returned(context):
 @given('a user edits a non-existing draft')
 def step_impl_user_edits_non_existant_draft(context):
     data.update({'msg_id': '001',
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -133,11 +156,12 @@ def step_impl_user_edits_non_existant_draft(context):
 # Scenario 3: A user edits a draft that has a too large to attribute
 @given("a user modifies a draft with a to attribute that is too big")
 def step_impl_modifies_draft_to_attribute_too_big(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -152,11 +176,12 @@ def step_impl_modifies_draft_to_attribute_too_big(context):
 # Scenario 4: A user edits a draft that has a too large from attribute
 @given("a user modifies a draft with a from attribute that is too big")
 def step_impl_user_modifies_draft_from_attribute_too_big(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -171,11 +196,12 @@ def step_impl_user_modifies_draft_from_attribute_too_big(context):
 # Scenario 5: A user edits a draft that has a too large body attribute
 @given("a user modifies a draft with a body attribute that is too big")
 def step_impl_user_modifies_draft_body_attribute_too_big(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -190,11 +216,12 @@ def step_impl_user_modifies_draft_body_attribute_too_big(context):
 # Scenario 6: A user edits a draft that has a too large subject attribute
 @given("a user modifies a draft with a subject attribute that is too big")
 def step_impl_user_modifies_draft_subject_attribute_too_big(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -209,13 +236,14 @@ def step_impl_user_modifies_draft_subject_attribute_too_big(context):
 # Scenario 7: A user edits a draft not including a to attribute
 @given("a user modifies a draft not adding a to attribute")
 def step_impl_user_modifies_draft_no_to_attribute(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
     context.etag = get_draft.headers.get('ETag')
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -224,19 +252,20 @@ def step_impl_user_modifies_draft_no_to_attribute(context):
                  'collection_exercise': 'collection exercise1',
                  'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
                  'survey': 'BRES'})
-    data['msg_to'] = ''
+    data.pop('msg_to')
 
 
 # Scenario 8: A user edits a draft not including a body attribute
 @given("a user modifies a draft not adding a body attribute")
 def step_impl_user_modifies_draft_no_body(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
     context.etag = get_draft.headers.get('ETag')
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -251,13 +280,14 @@ def step_impl_user_modifies_draft_no_body(context):
 # Scenario 9: A user edits a draft not including a subject attribute
 @given("a user modifies a draft not adding a subject attribute")
 def step_impl_user_modifies_draft_no_subject(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
     context.etag = get_draft.headers.get('ETag')
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -272,13 +302,14 @@ def step_impl_user_modifies_draft_no_subject(context):
 # Scenario 10: A user edits a draft not including a subject attribute
 @given("a user modifies a draft not adding a thread id attribute")
 def step_impluser_modifies_draft_no_thread_id(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
     context.etag = get_draft.headers.get('ETag')
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -293,11 +324,12 @@ def step_impluser_modifies_draft_no_thread_id(context):
 # Scenario 11: A user edits a draft where msg id in url and in the message body do not match
 @given("a user tries to modify a draft with mismatched msg ids")
 def step_impl_user_modifies_draft_with_mismatched_msg_id(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
     data.update({'msg_id': '0000-0000-0000-0000',
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -311,6 +343,7 @@ def step_impl_user_modifies_draft_with_mismatched_msg_id(context):
 # Scenario 12: A user is editing a draft while another user tries to modify the same draft
 @given("a draft message is being edited")
 def step_impl_draft_message_is_being_edited(context):
+        before_scenario(context)
         add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data),
                                            headers=headers)
         post_resp = json.loads(add_draft.data)
@@ -319,7 +352,7 @@ def step_impl_draft_message_is_being_edited(context):
                                           headers=headers)
         context.etag = get_draft.headers.get('ETag')
         data.update({'msg_id': context.msg_id,
-                     'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                     'msg_to': 'BRES',
                      'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                      'subject': 'test',
                      'body': 'test',
@@ -337,7 +370,7 @@ def step_impl_draft_message_is_being_edited(context):
 @when("another user tries to modify the same draft message")
 def step_impl_another_user_tries_to_modify_same_draft(context):
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'test',
@@ -361,6 +394,7 @@ def step_impl_conflict_returned(context):
 #  Scenario: User retrieves etag from the header when modifying a draft
 @given('there is a draft to be modified')
 def step_impl_there_is_a_draft_to_be_modified(context):
+    before_scenario(context)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data),
                                        headers=headers)
     post_resp = json.loads(add_draft.data)
@@ -373,7 +407,7 @@ def step_impl_there_is_a_draft_to_be_modified(context):
 @when('the user modifies the draft')
 def step_impl_the_user_modifies_the_draft(context):
     data.update({'msg_id':context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Edited',
@@ -402,7 +436,7 @@ def step_impl_new_etag_should_be_returned(context):
 @when('the user edits the draft without etag')
 def step_impl_user_saves_the_draft_without_etag(context):
     data.update({'msg_id': context.msg_id,
-                 'msg_to': 'ce12b958-2a5f-44f4-a6da-861e59070a31',
+                 'msg_to': 'BRES',
                  'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882',
                  'subject': 'test',
                  'body': 'Test',
@@ -420,6 +454,21 @@ def step_impl_user_saves_the_draft_without_etag(context):
 # Scenario: A user edits a previously saved draft without formatting
 @given('a user edits a previously saved draft without formatting')
 def step_impl_user_edits_saved_draft_no_formatting(context):
+    before_scenario(context)
+    add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
+    post_resp = json.loads(add_draft.data)
+    context.msg_id = post_resp['msg_id']
+    get_draft = app.test_client().get('http://localhost:5050/draft/{0}'.format(context.msg_id), headers=headers)
+    context.etag = get_draft.headers.get('ETag')
+    data.update(json.loads(get_draft.data))
+    data['body'] = 'replaced'
+
+
+# Scenario: A user edits a previously saved draft without formatting and msg_to
+@given('a user edits a previously saved draft without formatting and msg_to')
+def step_impl_user_edits_saved_draft_no_formatting(context):
+    before_scenario(context)
+    post_data.pop('msg_to', None)
     add_draft = app.test_client().post('http://localhost:5050/draft/save', data=json.dumps(post_data), headers=headers)
     post_resp = json.loads(add_draft.data)
     context.msg_id = post_resp['msg_id']
