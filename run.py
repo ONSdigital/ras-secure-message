d450e3f9b3c8d5d44b3c408c8d1611a8f580c107
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-15 12:29:20 +0100

    Update run script.

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 #!flask/bin/python
 import os
+import logging
 from app.application import app
+from app.cloud.cloud_foundry import ONSCloudFoundry
+from structlog import wrap_logger
 
-DEV_PORT = os.getenv('DEV_PORT', 5050)
-app.run(debug=True, host='0.0.0.0', port=int(DEV_PORT))
+logger = wrap_logger(logging.getLogger(__name__))
+
+if __name__ == '__main__':
+    cf = ONSCloudFoundry()
+    # First check if front-stage is deployed in a Cloud Foundry environment
+    if cf.detected:
+        port = cf.port
+        protocol = cf.protocol
+        logger.info('* Cloud Foundry environment detected.')
+        logger.info('* Cloud Foundry port "{}"'.format(port))
+        logger.info('* Cloud Foundry protocol "{}'.format(protocol))
+    else:
+        port = os.getenv('FS_DEV_PORT', 5001)
+    logger.info('* starting listening port "{}"'.format(port))
+    app.run(debug=True, host='0.0.0.0', port=int(port))
