6a89935aacc2e6e6141b756ea5d052cbfd20cfc8
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-15 13:58:36 +0100

    Add CF database logging.

--- run.py
+++ run.py
@@ -9,15 +9,16 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 if __name__ == '__main__':
     cf = ONSCloudFoundry()
-    # First check if front-stage is deployed in a Cloud Foundry environment
+    # First check if secure messaging is deployed in a Cloud Foundry environment
     if cf.detected:
         port = cf.port
         protocol = cf.protocol
         database = cf.get_service(tags='database')
         logger.info('* Cloud Foundry environment detected.')
         logger.info('* Cloud Foundry port "{}"'.format(port))
-        logger.info('* Cloud Foundry protocol "{}'.format(protocol))
+        logger.info('* Cloud Foundry protocol "{}"'.format(protocol))
+        logger.info('* Cloud Foundry database service "{}"'.format(database))
     else:
-        port = os.getenv('FS_DEV_PORT', 5050)
+        port = os.getenv('SMS_DEV_PORT', 5050)
     logger.info('* starting listening port "{}"'.format(port))
     app.run(debug=True, host='0.0.0.0', port=int(port))
