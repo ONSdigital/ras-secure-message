9f02cd34287c2145e066511eaf2240cf612309a3
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-15 16:04:16 +0100

    Monkey patch for cfenv match method to handle list.

--- app/cloud/cloud_foundry.py
+++ app/cloud/cloud_foundry.py
@@ -1,10 +1,14 @@
 import cfenv
+import re
+
+RegexType = type(re.compile(''))
 
 
 class ONSCloudFoundry(object):
 
     def __init__(self):
-
+        #   Monkey patch cfenv so it can handle a list match
+        cfenv.match = my_match
         self._cf_env = cfenv.AppEnv()
         self._host = self._cf_env.uris[0].split(':') if self.detected else 'localhost'
         self._protocol = 'https' if self.detected else 'http'
@@ -27,3 +31,17 @@ class ONSCloudFoundry(object):
 
     def database(self):
         return self._cf_env.get_service(tags='database')
+
+def my_match(target, pattern):
+        """
+        This is a customised version of "match" that also handles matching based
+        on 'target' possibly being a list.
+        """
+
+        if target is None:
+            return False
+        if isinstance(pattern, RegexType):
+            return pattern.search(target)
+        if isinstance(target, list):
+            return pattern in target
+        return pattern == target
