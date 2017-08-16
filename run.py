efeda6ce22dfe8a2ba5aa85acc8514350f282b88
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-16 17:27:11 +0100

    Rollback change to run.py as not needed for CF.

--- run.py
+++ run.py
@@ -1,21 +1,6 @@
 #!flask/bin/python
 import os
-import logging
 from app.application import app
-from app.cloud.cloud_foundry import ONSCloudFoundry
-from structlog import wrap_logger
 
-logger = wrap_logger(logging.getLogger(__name__))
-
-if __name__ == '__main__':
-    cf = ONSCloudFoundry()
-    # First check if secure messaging is deployed in a Cloud Foundry environment
-    if cf.detected:
-        port = cf.port
-        logger.info('* Cloud Foundry environment detected.')
-        logger.info('* Cloud Foundry port "{}"'.format(port))
-
-    else:
-        port = os.getenv('SMS_DEV_PORT', 5050)
-    logger.info('* starting listening port "{}"'.format(port))
-    app.run(debug=True, host='0.0.0.0', port=int(port))
+DEV_PORT = os.getenv('DEV_PORT', 5050)
+app.run(debug=True, host='0.0.0.0', port=int(DEV_PORT))
