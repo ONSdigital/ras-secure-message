2cfd4d6e358765831d269d2614e9687ae393d5a0
Emilio Ward <emilioward@hotmail.co.uk>
2017-08-17 13:57:31 +0100

    removed commented code and fixed travis

--- app/common/alerts.py
+++ app/common/alerts.py
@@ -22,48 +22,26 @@ class AlertViaGovNotify:
 class AlertUser:
     """Alert goes via gov notify (0) or via logs (1)"""
 
-    NOTIFICATION_LOGGING = settings.NOTIFICATION_LOGGING
-    logger.debug('Notification logging (0=disabled, 1=enabled) : {}'.format(NOTIFICATION_LOGGING))
+    NOTIFY_VIA_LOGGING = settings.NOTIFY_VIA_LOGGING
+    logger.debug('Notification logging (0=disabled, 1=enabled) : {}'.format(NOTIFY_VIA_LOGGING))
     alert_method = AlertViaGovNotify()
 
-    if NOTIFICATION_LOGGING == '1':
-        def __init__(self, alerter=None):
-            if alerter is not None:
-                self.alert_method = alerter
+    def __init__(self, alerter=None):
+        if alerter is not None:
+            self.alert_method = alerter
 
-        def send(self, email, reference):
+    def send(self, email, reference):
+        if self.NOTIFY_VIA_LOGGING == '1':
             try:
                 logger.info('email details {}, {}'.format(email, reference))
             except BaseException as e:
                 logger.exception(e)
             finally:
                 return 201, 'OK'
-    else:
-        def __init__(self, alerter=None):
-            if alerter is not None:
-                self.alert_method = alerter
-
-        def send(self, email, reference):
+        else:
             try:
                 self.alert_method.send(email, reference)
             except BaseException as e:
                 logger.exception(e)
             finally:
                 return 201, 'OK'
-
-
-# class AlertUser:
-#     """Alert User"""
-#     alert_method = AlertViaGovNotify()
-#
-#     def __init__(self, alerter=None):
-#         if alerter is not None:
-#             self.alert_method = alerter
-#
-#     def send(self, email, reference):
-#         try:
-#             self.alert_method.send(email, reference)
-#         except BaseException as e:
-#             logger.exception(e)
-#         finally:
-#             return 201, 'OK'
--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ NOTIFICATION_DEV_EMAIL = os.getenv('NOTIFICATION_DEV_EMAIL', 'emilio.ward@ons.go
 
 # LOGGING NOTIFICATION SETTINGS
 
-NOTIFICATION_LOGGING = os.getenv('NOTIFY_VIA_LOGGING', '0')
+NOTIFY_VIA_LOGGING = os.getenv('NOTIFY_VIA_LOGGING', '0')
 
 # SQLAlchemy configuration
 
@@ -37,7 +37,7 @@ SQLALCHEMY_POOL_SIZE = os.getenv('SQLALCHEMY_POOL_SIZE', None)
 
 
 SM_JWT_SECRET = os.getenv('SM_JWT_SECRET', 'vrwgLNWEffe45thh545yuby')
-SM_JWT_ENCRYPT = os.getenv('SM_JWT_ENCRYPT', '0')
+SM_JWT_ENCRYPT = os.getenv('SM_JWT_ENCRYPT', '1')
 
 #  Keys
 SM_USER_AUTHENTICATION_PRIVATE_KEY = open("{0}/jwt-test-keys/sm-user-authentication-encryption-private-key.pem".format(os.getenv('RAS_SM_PATH')))\
