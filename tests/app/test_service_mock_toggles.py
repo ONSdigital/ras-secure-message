34c145392a1dc90c5ac7a9d3201470b210e7c9e9
Andrew Torrance <Andrew.Torrance@Ons.Gov.uk>
2017-08-18 15:18:13 +0100

    Added log call tests back in

--- tests/app/test_service_mock_toggles.py
+++ tests/app/test_service_mock_toggles.py
@@ -4,7 +4,9 @@ from app.services.service_toggles import Party, Case
 from app.api_mocks.party_service_mock import PartyServiceMock
 from app.services.party_service import PartyService
 import logging
+from structlog import wrap_logger
 
+logger = wrap_logger(logging.getLogger(__name__))
 
 class ServiceMockTestCase(unittest.TestCase):
     """Test case for toggling between a service and its mock"""
@@ -29,3 +31,32 @@ class ServiceMockTestCase(unittest.TestCase):
         sut.use_real_service()
         sut.use_mock_service()
         self.assertTrue(isinstance(sut._service, PartyServiceMock))
+
+    @patch.object(logger, 'debug')
+    def test_changing_to_non_mocked_party_service_results_calls_logger(self, logger_debug):
+        """Validate that the logger is called if real party service is used"""
+        sut = Party(False)
+        sut.use_real_service()
+        logger_debug.assert_called()
+
+    @patch.object(logger, 'debug')
+    def test_changing_to_mocked_party_service_results_calls_logger(self, logger_debug):
+        """Validate that the logger is called if mocked party service is used"""
+        sut = Party(False)
+        sut.use_mock_service()
+        logger_debug.assert_called()
+
+    @patch.object(logger, 'debug')
+    def test_changing_to_non_mocked_case_service_results_calls_logger(self, logger_debug):
+        """Validate that the logger is called if real party service is used"""
+        sut = Case(False)
+        sut.use_real_service()
+        logger_debug.assert_called()
+
+    @patch.object(logger, 'debug')
+    def test_changing_to_mocked_case_service_results_calls_logger(self, logger_debug):
+        """Validate that the logger is called if mocked party service is used"""
+
+        sut = Case(False)
+        sut.use_mock_service()
+        logger_debug.assert_called()
