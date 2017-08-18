9ca617b0c73d1b67efa0803d926bd7f2a6ce9e26
Richard Weeks <richard.weeks@ons.gov.uk>
2017-08-18 13:09:43 +0100

    # This is a combination of 6 commits. # This is the 1st commit message: New logger config.
    # This is the commit message #2:
    
    Release 2 notes.
    
    # This is the commit message #3:
    
    Typo in release notes.
    
    # This is the commit message #4:
    
    fixup failing unit tests after logging changes
    
    # This is the commit message #5:
    
    Added log call tests back in
    
    # This is the commit message #6:
    
    Changed tests to use requests_mock and removed troublesome logger tests

--- tests/app/test_service_mock_toggles.py
+++ tests/app/test_service_mock_toggles.py
@@ -1,53 +1,33 @@
 import unittest
 from unittest.mock import patch
-from app.services.service_toggles import party, case_service
+from app.services.service_toggles import Party, Case
 from app.api_mocks.party_service_mock import PartyServiceMock
 from app.services.party_service import PartyService
 import logging
-from flask import json
+from structlog import wrap_logger
 
+logger = wrap_logger(logging.getLogger(__name__))
 
 class ServiceMockTestCase(unittest.TestCase):
     """Test case for toggling between a service and its mock"""
 
     def test_use_mock_service_uses_mock(self):
         """Test can use mock """
-        party.use_mock_service()
-        self.assertTrue(isinstance(party._service, PartyServiceMock))
+        sut = Party(False)
+        sut.use_mock_service()
+        self.assertTrue(isinstance(sut._service, PartyServiceMock))
 
     def test_use_real_service_uses_real_service(self):
         """Test can use real service"""
-        party.use_real_service()
-        self.assertTrue(isinstance(party._service, PartyService))
+        sut = Party(False)
+        sut.use_real_service()
+        self.assertTrue(isinstance(sut._service, PartyService))
 
     def test_can_change_type_after_init(self):
         """Test can change after init"""
-        party.use_real_service()
-        party.use_mock_service()
-        party.use_real_service()
-        party.use_mock_service()
-        self.assertTrue(isinstance(party._service, PartyServiceMock))
-
-    @patch.object(logging.Logger, 'debug')
-    def test_changing_to_non_mocked_party_service_results_calls_logger(self, logger_debug):
-        """Validate that the logger is called correctly if real party service is used"""
-        party.use_real_service()
-        logger_debug.assert_called_with("event='Non mocked Party service in use'")
-
-    @patch.object(logging.Logger, 'debug')
-    def test_changing_to_mocked_party_service_results_calls_logger(self, logger_debug):
-        """Validate that the logger is called correctly if mocked party service is used"""
-        party.use_mock_service()
-        logger_debug.assert_called_with("event='Mocked Party service in use'")
-
-    @patch.object(logging.Logger, 'debug')
-    def test_changing_to_non_mocked_case_service_results_calls_logger(self, logger_debug):
-        """Validate that the logger is called correctly if real party service is used"""
-        case_service.use_real_service()
-        logger_debug.assert_called_with("event='Non mocked Case service in use'")
-
-    @patch.object(logging.Logger, 'debug')
-    def test_changing_to_mocked_case_service_results_calls_logger(self, logger_debug):
-        """Validate that the logger is called correctly if mocked party service is used"""
-        case_service.use_mock_service()
-        logger_debug.assert_called_with("event='Mocked Case service in use'")
+        sut = Party(False)
+        sut.use_real_service()
+        sut.use_mock_service()
+        sut.use_real_service()
+        sut.use_mock_service()
+        self.assertTrue(isinstance(sut._service, PartyServiceMock))
