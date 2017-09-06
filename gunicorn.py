340f210092ebbccd384681e32d904a4f85e534b2
Richard Weeks <richard.weeks@ons.gov.uk>
2017-09-06 11:54:53 +0100

    Info level logging for Gunicorn.

--- gunicorn.py
+++ gunicorn.py
@@ -142,7 +142,7 @@ tmp_upload_dir = None
 #
 
 errorlog = '-'
-loglevel = 'debug'
+loglevel = 'info'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 
