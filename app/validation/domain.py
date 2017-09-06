f93216791dade19e8905be5825c558bfe2be5334
Richard Weeks <richard.weeks@ons.gov.uk>
2017-09-06 13:36:48 +0100

    Remove duplicate in log statement.

--- app/validation/domain.py
+++ app/validation/domain.py
@@ -80,7 +80,7 @@ class MessageSchema(Schema):
             logger.error('Internal user not authorised to send a message on behalf of user or work group', message_from=msg_from)
             raise ValidationError('You are not authorised to send a message on behalf of user or work group {0}'.format(msg_from))
         if g.user.is_respondent and msg_from != g.user.user_uuid:
-            logger.error('Respondent not authorised to send a message on behalf of user or work group', message_from=msg_from, user_uuid=g.user.user_uuid, msg_from=msg_from)
+            logger.error('Respondent not authorised to send a message on behalf of user or work group', message_from=msg_from, user_uuid=g.user.user_uuid)
             raise ValidationError('You are not authorised to send a message on behalf of user or work group {0}'.format(msg_from))
 
     @validates("body")
