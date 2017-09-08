d6290c668e95bb40763cfb2d9c54d2388f952852
Andrew Millar <andrew.millar@qa.com>
2017-09-08 11:21:34 +0100

    moving required testing config to run_tests, removing them from travis

--- run_tests.py
+++ run_tests.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ['JWT_SECRET'] = 'vrwgLNWEffe45thh545yuby'
-    os.environ['SECURITY_USER_NAME'] = 'dummy_user'
-    os.environ['SECURITY_USER_PASSWORD'] = 'dummy_password'
+    os.environ['JWT_SECRET'] = 'testsecret'
+    os.environ['SECURITY_USER_NAME'] = 'test_user'
+    os.environ['SECURITY_USER_PASSWORD'] = 'test_password'
+    os.environ['NOTIFY_VIA_LOGGING'] = '1'
 
     from behave import __main__ as behave_executable
     behave = behave_executable.main()
