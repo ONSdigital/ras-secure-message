dd9f0be5c771eb5e7d885197c2909cb7afe8cab3
James Gardiner <james@jgardiner.co.uk>
2018-01-26 12:55:41 +0000

    Remove print call

--- secure_message/authentication/jwt.py
+++ secure_message/authentication/jwt.py
@@ -11,8 +11,7 @@ def encode(data):
     Function to encode python dict data
     """
     jwt_algorithm = current_app.config['SM_JWT_ALGORITHM']
-    jwt_secret = current_app.config['SM_JWT_SECRET']
-    print(data)
+    jwt_secret = current_app.config['JWT_SECRET']
     return jwt.encode(data, jwt_secret, algorithm=jwt_algorithm, headers={"alg": jwt_algorithm, 'typ': 'jwt'})
 
 
@@ -21,6 +20,6 @@ def decode(token):
     Function to decode python dict data
     """
     jwt_algorithm = current_app.config['SM_JWT_ALGORITHM']
-    jwt_secret = current_app.config['SM_JWT_SECRET']
+    jwt_secret = current_app.config['JWT_SECRET']
 
     return jwt.decode(token, jwt_secret, algorithms=[jwt_algorithm])
