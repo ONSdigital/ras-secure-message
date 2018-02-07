cc114ea776df245aa04147a57b03e07dc9524efb
Gemma Irving <gemma.irving@ons.gov.uk>
2018-02-07 14:29:24 +0000

    Remove unnecessary test

--- tests/app/test_logging.py
+++ tests/app/test_logging.py
@@ -35,12 +35,5 @@ class LoggingTestCase(unittest.TestCase):
         output = out.getvalue().strip()
         self.assertIsNotNone(output)
 
-    def test_basic_logger_config(self):
-        """Test logger configuration"""
-        with mock.patch('logging.basicConfig') as loggingConfig:
-            logger_initial_config(service_name='ras-secure-message', log_level='INFO', logger_format="message", logger_date_format='2017-06-13')
-            loggingConfig.assert_called_with(level='INFO', format="message")
-
-
 if __name__ == '__main__':
     unittest.main()
