a7ddda262b6c14398a8e38f1739cacf2a893d95d
dcdarrell9 <darrells_king@hotmail.com>
2018-03-21 09:04:00 +0000

    Fix failing health monitor test

--- tests/app/test_health_monitor.py
+++ tests/app/test_health_monitor.py
@@ -44,7 +44,6 @@ class HealthTestCase(unittest.TestCase):
         details = {'Name': '',
                    'Version': '',
                    'SMS Log level': '',
-                   'APP Log Level': '',
                    'API Functionality': '',
                    'Using party service mock': '',
                    'SM JWT ENCRYPT': '',
