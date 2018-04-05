a63b30fedd881da18beb2b09d77b6c01d78d7c51
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-04-05 13:39:04 +0100

    Update unit tests for changes

--- tests/app/test_utilities.py
+++ tests/app/test_utilities.py
@@ -4,6 +4,6 @@ from secure_message.common.utilities import MessageArgs
 BRES_SURVEY = "33333333-22222-3333-4444-88dc018a1a4c"
 
 
-def get_args(page=1, limit=100, survey="", cc="", ru="", label="", desc=True, ce=""):
-    return MessageArgs(page=page, limit=limit, ru_id=ru, surveys=list(survey), cc=cc, label=label,
+def get_args(page=1, limit=100, surveys=[], cc="", ru="", label="", desc=True, ce=""):
+    return MessageArgs(page=page, limit=limit, ru_id=ru, surveys=surveys, cc=cc, label=label,
                        desc=desc, ce=ce)
