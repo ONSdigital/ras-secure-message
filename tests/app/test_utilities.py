574ab2c6b3bab686101f14865d12e6f5d4f23c1e
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-04-05 14:13:44 +0100

    Fixed quality issue

--- tests/app/test_utilities.py
+++ tests/app/test_utilities.py
@@ -4,6 +4,6 @@ from secure_message.common.utilities import MessageArgs
 BRES_SURVEY = "33333333-22222-3333-4444-88dc018a1a4c"
 
 
-def get_args(page=1, limit=100, surveys=[], cc="", ru="", label="", desc=True, ce=""):
+def get_args(page=1, limit=100, surveys=None, cc="", ru="", label="", desc=True, ce=""):
     return MessageArgs(page=page, limit=limit, ru_id=ru, surveys=surveys, cc=cc, label=label,
                        desc=desc, ce=ce)
