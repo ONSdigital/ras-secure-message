3f523d20e1c6631db400ddd2f3ae902dc63f4872
Adam Mann <manny160389@hotmail.com>
2018-06-13 15:51:46 +0100

    Add remaining utility tests

--- tests/app/test_utilities.py
+++ tests/app/test_utilities.py
@@ -32,7 +32,7 @@ class UtilitiesTestCase(unittest.TestCase):
                                 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                                 'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'],
                                 'read_date': '2018-06-05 15:23:39.898317',
-                                'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251',
+                                'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
                                 'sent_date': '2018-06-05 15:23:38.025084',
                                 'subject': 'Message to ONS',
                                 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54',
@@ -46,7 +46,7 @@ class UtilitiesTestCase(unittest.TestCase):
                                 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                                 'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'],
                                 'read_date': '2018-06-05 15:23:39.898317',
-                                'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251',
+                                'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
                                 'sent_date': '2018-06-05 15:23:38.025084',
                                 'subject': 'Message to ONS',
                                 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54',
@@ -61,7 +61,7 @@ class UtilitiesTestCase(unittest.TestCase):
                                 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                                 'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'],
                                 'read_date': '2018-06-05 15:23:39.898317',
-                                'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251',
+                                'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
                                 'sent_date': '2018-06-05 15:23:38.025084',
                                 'subject': 'Message to ONS',
                                 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54',
@@ -75,7 +75,7 @@ class UtilitiesTestCase(unittest.TestCase):
                                 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                                 'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'],
                                 'read_date': '2018-06-05 15:23:39.898317',
-                                'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251',
+                                'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc',
                                 'sent_date': '2018-06-05 15:23:38.025084',
                                 'subject': 'Message to ONS',
                                 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54',
@@ -86,21 +86,80 @@ class UtilitiesTestCase(unittest.TestCase):
     external_user_record = {"id": "0a7ad740-10d5-4ecb-b7ca-3c0384afb882", "firstName": "Vana", "lastName": "Oorschot",
                             "emailAddress": "vana123@aol.com", "telephone": "+443069990289", "status": "ACTIVE", "sampleUnitType": "BI"}
 
-    @unittest.skip("rework")
-    def test_get_business_details(self):
+    business_record = {'id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'name': 'Apple'}
+
+    def test_get_business_details_with_external_message_first(self):
+
+        populated_message = [{'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
+                              'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@ru_id': {'id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'name': 'Apple'}},
+                             {'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
+                              'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@ru_id': {'id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'name': 'Apple'}}]
+        messages_business_external_first_copy = copy.deepcopy(self.messages_external_first)
+
+        for message in messages_business_external_first_copy:
+            self.assertNotIn('@ru_id', message)
+        with self.app.app_context():
+            result = add_business_details(messages_business_external_first_copy)
+            for message in messages_business_external_first_copy:
+                self.assertIn('@ru_id', message)
+                self.assertEqual(message['@ru_id'], self.business_record)
+
+            self.assertEqual(result[0], populated_message[0])
+            self.assertEqual(result[1], populated_message[1])
+
+    def test_get_business_details_with_internal_message_first(self):
+
+        populated_message = [{'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
+                              'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@ru_id': {'id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'name': 'Apple'}},
+                             {'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
+                              'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@ru_id': {'id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'name': 'Apple'}}]
+
         messages_business_internal_first_copy = copy.deepcopy(self.messages_internal_first)
 
+        for message in messages_business_internal_first_copy:
+            self.assertNotIn('@ru_id', message)
         with self.app.app_context():
             result = add_business_details(messages_business_internal_first_copy)
-            print(result)
-            self.assertEqual(result, [])
+
+            for message in messages_business_internal_first_copy:
+                self.assertIn('@ru_id', message)
+                self.assertEqual(message['@ru_id'], self.business_record)
+
+            self.assertEqual(result[0], populated_message[0])
+            self.assertEqual(result[1], populated_message[1])
 
     def test_get_from_details_with_external_message_first(self):
 
         populated_message = [{'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
-                              'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513', 'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317', 'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS', 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6', '@msg_from': {'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot', 'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}},
+                              'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@msg_from': {'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot',
+                                            'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}},
                              {'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
-                              'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513', 'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317', 'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS', 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6', '@msg_from': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred', 'lastName': 'flinstone', 'emailAddress': 'mock@email.com'}}]
+                              'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@msg_from': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred', 'lastName': 'flinstone',
+                                            'emailAddress': 'mock@email.com'}}]
 
         messages_from_external_first_copy = copy.deepcopy(self.messages_external_first)
         for message in messages_from_external_first_copy:
@@ -120,8 +179,20 @@ class UtilitiesTestCase(unittest.TestCase):
 
     def test_get_from_details_with_internal_message_first(self):
 
-        populated_message = [{'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True, 'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513', 'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317', 'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS', 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6', '@msg_from': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred', 'lastName': 'flinstone', 'emailAddress': 'mock@email.com'}},
-                             {'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False, 'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513', 'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317', 'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS', 'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6', '@msg_from': {'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot', 'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}}]
+        populated_message = [{'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
+                              'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@msg_from': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred', 'lastName': 'flinstone',
+                                            'emailAddress': 'mock@email.com'}},
+                             {'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
+                              'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
+                              'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
+                              '@msg_from': {'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot',
+                                            'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}}]
 
         messages_from_internal_first_copy = copy.deepcopy(self.messages_internal_first)
         for message in messages_from_internal_first_copy:
@@ -142,14 +213,14 @@ class UtilitiesTestCase(unittest.TestCase):
         populated_message = [{'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
                               'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                               'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
-                              'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
                               'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
                               '@msg_to': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred',
                                           'lastName': 'flinstone', 'emailAddress': 'mock@email.com'}},
                              {'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
                               'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                               'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
-                              'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
                               'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
                               '@msg_to': [{'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot',
                                            'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}]}]
@@ -174,14 +245,14 @@ class UtilitiesTestCase(unittest.TestCase):
         populated_message = [{'body': 'Reply body from internal user', 'collection_case': '', 'collection_exercise': '', 'from_internal': True,
                               'labels': ['INBOX'], 'msg_from': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                               'msg_to': ['0a7ad740-10d5-4ecb-b7ca-3c0384afb882'], 'read_date': '2018-06-05 15:23:39.898317',
-                              'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
                               'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
                               '@msg_to': [{'id': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'firstName': 'Vana', 'lastName': 'Oorschot',
                                            'emailAddress': 'vana123@aol.com', 'telephone': '+443069990289', 'status': 'ACTIVE', 'sampleUnitType': 'BI'}]},
                              {'body': 'Reply body from respondent', 'collection_case': '', 'collection_exercise': '', 'from_internal': False,
                               'labels': ['INBOX'], 'msg_from': '0a7ad740-10d5-4ecb-b7ca-3c0384afb882', 'msg_id': '048ffdb5-18f0-46f0-bfa0-ea298521c513',
                               'msg_to': ['01b51fcc-ed43-4cdb-ad1c-450f9986859b'], 'read_date': '2018-06-05 15:23:39.898317',
-                              'ru_id': '6c141ebb-10d1-4065-ac3e-ef5b5c95d251', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
+                              'ru_id': 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc', 'sent_date': '2018-06-05 15:23:38.025084', 'subject': 'Message to ONS',
                               'survey': 'cb8accda-6118-4d3b-85a3-149e28960c54', 'thread_id': '53a430f1-de21-4279-b17e-1bfb4c4813a6',
                               '@msg_to': {'id': '01b51fcc-ed43-4cdb-ad1c-450f9986859b', 'firstName': 'fred', 'lastName': 'flinstone',
                                           'emailAddress': 'mock@email.com'}}]
