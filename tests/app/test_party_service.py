c9c43ea43aac23ce6c5a9f0bf10feb2e23bfe70a
Adam Mann <manny160389@hotmail.com>
2018-06-20 16:31:51 +0100

    Update docstrings

--- tests/app/test_party_service.py
+++ tests/app/test_party_service.py
@@ -23,7 +23,7 @@ class PartyTestCase(unittest.TestCase):
     # Get business details tests
     @requests_mock.mock()
     def test_get_business_details_single_id_success(self, mock_request):
-        """Test get business details sends a request and returns data"""
+        """Test get business details with a single uuid"""
         ru_ids = ["b08c07c3-df28-4283-bb4c-c048729ce372"]
         business_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/businesses?id={ru_ids[0]}"
         mock_request.get(business_data_url, status_code=200, reason="OK", text='[{"response": "data"}]')
@@ -35,7 +35,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_business_details_multiple_id_success(self, mock_request):
-        """Test get business details sends a request and returns data"""
+         """Test get business details with a multiple uuids at once"""
         ru_ids = ["c614e64e-d981-4eba-b016-d9822f09a4fb", "c614e64e-d981-4eba-b016-d9822f09a4f2"]
         business_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/businesses?id={ru_ids[0]}&id={ru_ids[1]}"
         mock_request.get(business_data_url, status_code=200, reason="OK", text='[{"response": "data"}]')
@@ -46,7 +46,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_multiple_business_uuid_one_invalid(self, mock_request):
-        """Test get business details sends a request and returns data"""
+        """Test get business details fails and returns an empty list when a non-uuid is sent"""
         ru_ids = ["not_a_uuid"]
         business_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/businesses?id={ru_ids[0]}"
         mock_request.get(business_data_url, status_code=400, reason="Invalid uuid", text='{"error": "text"}')
@@ -58,7 +58,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_business_details_unauthorised_failure(self, mock_request):
-        """Test get business details and returns correctly from a list"""
+        """Test get business details fails and returns and empty list when the client is unauthorised to access the party service"""
         ru_ids = ["1234"]
         business_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/businesses?id={ru_ids[0]}"
         mock_request.get(business_data_url, status_code=401, reason="unauthorised", text='Unauthorized Access')
@@ -71,7 +71,7 @@ class PartyTestCase(unittest.TestCase):
     # Get user (singular) details tests
     @requests_mock.mock()
     def test_get_user_details_success(self, mock_request):
-        """Test get user details sends a request and receives back data"""
+        """Test get user details with a single uuid"""
         user_id = "c614e64e-d981-4eba-b016-d9822f09a4fb"
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_id}"
         mock_request.get(user_data_url, status_code=200, reason="OK", text='{"Test": "test"}')
@@ -82,7 +82,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_user_details_invalid_uuid(self, mock_request):
-        """Test get business details sends a request and returns data"""
+        """Test get user details fails and returns an empty list when a non-uuid is sent"""
         user_id = "not_a_uuid"
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_id}"
         mock_request.get(user_data_url, status_code=400, reason="Invalid uuid", text='{"error": "text"}')
@@ -94,7 +94,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_user_details_unauthorised_failure(self, mock_request):
-        """Test get user details sends a request and receives back data"""
+        """Test get user details fails and returns and empty list when the client is unauthorised to access the party service"""
         user_ids = ['NotBres']
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_ids[0]}"
         mock_request.get(user_data_url, status_code=401, reason="unauthorised", text="Unauthorized access")
@@ -108,7 +108,7 @@ class PartyTestCase(unittest.TestCase):
     # Get users (plural) details tests
     @requests_mock.mock()
     def test_get_users_details_single_id_success(self, mock_request):
-        """Test get user details sends a request and receives back data"""
+        """Test get users details sends a request and receives back data"""
         user_ids = ["c614e64e-d981-4eba-b016-d9822f09a4fb"]
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_ids[0]}"
         mock_request.get(user_data_url, status_code=200, reason="OK", text='[{"response": "data"}]')
@@ -120,7 +120,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_users_details_multiple_id_success(self, mock_request):
-        """Test get business details sends a request and returns data"""
+       """Test get users details with a multiple uuids at once"""
         user_ids = ["c614e64e-d981-4eba-b016-d9822f09a4fb", "c614e64e-d981-4eba-b016-d9822f09a4f2"]
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_ids[0]}&id={user_ids[1]}"
         mock_request.get(user_data_url, status_code=200, reason="OK", text='[{"response": "data"}]')
@@ -131,7 +131,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_users_details_invalid_uuid(self, mock_request):
-        """Test get business details sends a request and returns data"""
+       """Test get users details fails and returns an empty list when a non-uuid is sent"""
         user_ids = ["not_a_uuid"]
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_ids[0]}"
         mock_request.get(user_data_url, status_code=400, reason="Invalid uuid", text='{"error": "text"}')
@@ -143,7 +143,7 @@ class PartyTestCase(unittest.TestCase):
 
     @requests_mock.mock()
     def test_get_users_details_unauthorised_failure(self, mock_request):
-        """Test get user details sends a request and receives back data"""
+        """Test get users details fails and returns and empty list when the client is unauthorised to access the party service"""
         user_ids = ['1234', '4567']
         user_data_url = f"{self.app.config['RAS_PARTY_SERVICE']}party-api/v1/respondents?id={user_ids[0]}&id={user_ids[1]}"
         mock_request.get(user_data_url, status_code=401, reason="unauthorised", text="Unauthorized access")
