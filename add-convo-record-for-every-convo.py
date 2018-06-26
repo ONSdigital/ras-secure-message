c11c5f2833dcda4d23dd10ce72e3aa47e7887d05
Mohammad Islam <MIslam92@users.noreply.github.com>
2018-06-26 15:03:19 +0100

    script to add convo records for every conversation (#216)

---
+++ add-convo-record-for-every-convo.py
@@ -0,0 +1,24 @@
+from sqlite3 import DatabaseError
+import sys
+
+from sqlalchemy import create_engine, insert
+from sqlalchemy.orm import sessionmaker
+
+from config import Config
+from secure_message.repository.database import Conversation, SecureMessage
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+Session = sessionmaker(bind=engine)
+db = Session()
+
+try:
+    subquery = db.query(Conversation.id)
+    distinct_ids = db.query(SecureMessage.thread_id).filter(~SecureMessage.thread_id.in_(subquery)).distinct()
+    for thread_id in distinct_ids:
+        db.execute(insert(Conversation).values({'id': thread_id, 'is_closed': 'False'}))
+        print('Added thread_id: {}'.format(thread_id[0]))
+    db.commit()
+except DatabaseError:
+    db.rollback()
+    print(DatabaseError)
+    sys.exit('Exiting')
