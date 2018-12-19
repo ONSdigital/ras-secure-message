0e1b1397297bb09fe08584f1dc2bde3fee47b2c5
Ryan Grundy <ryan.grundy@ons.gov.uk>
2018-12-19 12:41:38 +0000

    Message url now sent to notify gateway

--- secure_message/common/alerts.py
+++ secure_message/common/alerts.py
@@ -14,32 +14,35 @@ class AlertViaGovNotify:
     """Notify Api handler"""
 
     @staticmethod
-    def send(email, msg_id, survey_id, party_id):
+    def send(email, msg_id, personalisation, survey_id, party_id):
 
         notification = {
             "emailAddress": email,
+            "personalisation": personalisation,
             "reference": msg_id
         }
 
-        url = urlparse.urljoin(current_app.config['RM_NOTIFY_GATEWAY_URL'], current_app.config['NOTIFICATION_TEMPLATE_ID'])
+        url = urlparse.urljoin(current_app.config['RM_NOTIFY_GATEWAY_URL'],
+                               current_app.config['NOTIFICATION_TEMPLATE_ID'])
 
-        response = requests.post(url, auth=current_app.config['BASIC_AUTH'], timeout=current_app.config['REQUESTS_POST_TIMEOUT'],
-                                 json=notification)
+        response = requests.post(url, auth=current_app.config['BASIC_AUTH'],
+                                 timeout=current_app.config['REQUESTS_POST_TIMEOUT'], json=notification)
 
         # If a 500 error does occur, it won't be shown to the user and the exception will just be swallowed
         if response.status_code != 201:
             raise RasNotifyException(code=500, survey_id=survey_id, party_id=party_id)
         else:
             logger.info('Sent secure message email notification, via RM Notify-Gateway to GOV.UK Notify.',
-                        message_id=response.json()["id"], survey_id=survey_id, party_id=party_id)
+                        message_id=response.json()["id"], survey_id=survey_id, party_id=party_id,
+                        personalisation=personalisation)
 
 
 class AlertViaLogging:
     """Alert goes via gov notify (0) or via logs (1)"""
 
     @staticmethod
-    def send(_, msg_id, survey_id, party_id):
-        logger.info('Email sent', msg_id=msg_id, survey=survey_id, party_id=party_id)
+    def send(_, msg_id, personalisation, survey_id, party_id):
+        logger.info('Email sent', msg_id=msg_id, personalisation=personalisation, survey=survey_id, party_id=party_id)
 
 
 class AlertUser:
@@ -49,5 +52,5 @@ class AlertUser:
         if alerter is not None:
             self.alert_method = alerter
 
-    def send(self, email, msg_id, survey_id=None, party_id=None):
-        self.alert_method.send(email, msg_id, survey_id=survey_id, party_id=party_id)
+    def send(self, email, msg_id, personalisation, survey_id=None, party_id=None):
+        self.alert_method.send(email, msg_id, personalisation, survey_id=survey_id, party_id=party_id)
