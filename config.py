c2ebb4611476b1e68f8a761c6a96e27ebf7b8a49
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2019-03-14 13:31:35 +0000

    Update version to 0.14.0 (#247)

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     All configuration may be overridden by setting the appropriate environment variable name.
     """
     NAME = os.getenv('NAME', 'ras-secure-message')
-    VERSION = os.getenv('VERSION', '0.12.0')
+    VERSION = os.getenv('VERSION', '0.14.0')
 
     SECURE_MESSAGING_DATABASE_URL = os.getenv(
         'SECURE_MESSAGING_DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432')
