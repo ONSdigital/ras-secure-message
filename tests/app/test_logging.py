18f0b197cd2693a74775e72844d05876e2534d8d
anwilkie <47449873+anwilkie@users.noreply.github.com>
2019-07-23 13:40:22 +0100

    Getting rid of warnings (#256)
    * tidied up test

--- tests/app/test_logging.py
+++ tests/app/test_logging.py
@@ -4,18 +4,21 @@ from io import StringIO
 
 from flask import g
 
+from secure_message import constants
 from secure_message.validation.domain import MessageSchema
 from secure_message.application import create_app
 from secure_message.validation.user import User
 
-
 sys.path.append('../../ras-secure-message')
-
 saved_stdout = sys.stdout
 
 
 class LoggingTestCase(unittest.TestCase):
     """Test case for logging"""
+    ru_ref = 'f1a5e99c-8edf-489a-9c72-6cabe6c387fc'
+    survey_id = 'cb8accda-6118-4d3b-85a3-149e28960c54'
+    msg_from = 'Gemma'
+
     def setUp(self):
         """creates a test client"""
         self.app = create_app()
@@ -24,10 +27,10 @@ class LoggingTestCase(unittest.TestCase):
         """logging message endpoint"""
         out = StringIO()
         sys.stdout = out
-        message = {'msg_to': 'richard', 'msg_from': 'torrance', 'subject': 'hello', 'body': 'hello world',
-                   'thread_id': ''}
+        message = {'msg_to': [constants.NON_SPECIFIC_INTERNAL_USER], 'msg_from': self.msg_from, 'subject': 'hello', 'body': 'hello world',
+                   'thread_id': '', 'ru_id': self.ru_ref, 'survey': self.survey_id}
         with self.app.app_context():
-            g.user = User('torrence', 'respondent')
+            g.user = User('Gemma', 'respondent')
             schema = MessageSchema()
             schema.load(message)
         output = out.getvalue().strip()
