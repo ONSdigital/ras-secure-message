1cb46e99aecbdccd0b9be45c11bba9923d6444fb
Adam Mann <manny160389@hotmail.com>
2019-11-13 14:32:23 +0000

    Cannot reply to deleted message

--- secure_message/resources/messages.py
+++ secure_message/resources/messages.py
@@ -3,7 +3,7 @@ import logging
 from flask import request, jsonify, g, current_app, make_response
 from flask_restful import Resource
 from structlog import wrap_logger
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, NotFound
 
 from secure_message import constants
 from secure_message.common.alerts import AlertUser, AlertViaGovNotify, AlertViaLogging
@@ -32,15 +32,24 @@ class MessageSend(Resource):
             logger.info('Request must set accept content type "application/json" in header.')
         post_data = request.get_json(force=True)
 
+        logger.info("Here!!!!!!!!!--------------!!!!!!!!!here")
+        logger.info(post_data)
+
         if 'msg_id' in post_data:
             raise BadRequest(description="Message can not include msg_id")
         if post_data.get('thread_id'):
+            if g.user.is_internal and not party.get_users_details(post_data['msg_to']):
+                # If an internal person is sending a message to a respondent, we need to check that they exist.
+                # If they don't exist (because they've been deleted) then we raise a NotFound exception as the
+                # respondent can't be found in the system.
+                raise NotFound(description="Respondent not found")
             conversation_metadata = Retriever.retrieve_conversation_metadata(post_data.get('thread_id'))
             # Ideally, we'd return a 404 if there isn't a record in the conversation table.  But until we
             # ensure there is a record in here for every thread_id in the secure_message table, we just have to
             # assume that it's fine if it's empty.
             if conversation_metadata and conversation_metadata.is_closed:
                 raise BadRequest(description="Cannot reply to a closed conversation")
+
 
         post_data['from_internal'] = g.user.is_internal
         message = self._validate_post_data(post_data)
