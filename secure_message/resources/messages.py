1740a19507011dc5d00de2fb88d530be2f5b0937
Adam Mann <manny160389@hotmail.com>
2019-11-15 14:47:31 +0000

    Reorganise validation code

--- secure_message/resources/messages.py
+++ secure_message/resources/messages.py
@@ -32,21 +32,6 @@ class MessageSend(Resource):
             logger.info('Request must set accept content type "application/json" in header.')
         post_data = request.get_json(force=True)
 
-        if 'msg_id' in post_data:
-            raise BadRequest(description="Message can not include msg_id")
-        if post_data.get('thread_id'):
-            if g.user.is_internal and not party.get_users_details(post_data['msg_to']):
-                # If an internal person is sending a message to a respondent, we need to check that they exist.
-                # If they don't exist (because they've been deleted) then we raise a NotFound exception as the
-                # respondent can't be found in the system.
-                raise NotFound(description="Respondent not found")
-            conversation_metadata = Retriever.retrieve_conversation_metadata(post_data.get('thread_id'))
-            # Ideally, we'd return a 404 if there isn't a record in the conversation table.  But until we
-            # ensure there is a record in here for every thread_id in the secure_message table, we just have to
-            # assume that it's fine if it's empty.
-            if conversation_metadata and conversation_metadata.is_closed:
-                raise BadRequest(description="Cannot reply to a closed conversation")
-
         post_data['from_internal'] = g.user.is_internal
         message = self._validate_post_data(post_data)
 
@@ -83,7 +68,24 @@ class MessageSend(Resource):
 
     @staticmethod
     def _validate_post_data(post_data):
+        if 'msg_id' in post_data:
+            raise BadRequest(description="Message can not include msg_id")
+
         message = MessageSchema().load(post_data)
+
+        if post_data.get('thread_id'):
+            if post_data['from_internal'] and not party.get_users_details(post_data['msg_to']):
+                # If an internal person is sending a message to a respondent, we need to check that they exist.
+                # If they don't exist (because they've been deleted) then we raise a NotFound exception as the
+                # respondent can't be found in the system.
+                raise NotFound(description="Respondent not found")
+
+            conversation_metadata = Retriever.retrieve_conversation_metadata(post_data.get('thread_id'))
+            # Ideally, we'd return a 404 if there isn't a record in the conversation table.  But until we
+            # ensure there is a record in here for every thread_id in the secure_message table, we just have to
+            # assume that it's fine if it's empty.
+            if conversation_metadata and conversation_metadata.is_closed:
+                raise BadRequest(description="Cannot reply to a closed conversation")
         return message
 
     @staticmethod
