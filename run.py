8f7c36abcef10b2591d947a6605a1158a2a657dc
David Morgan <davidmorgan@ext.ons.gov.uk>
2021-10-14 16:51:37 +0100

    add changed

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from secure_message.application import create_app
 
 DEV_PORT = os.getenv("DEV_PORT", 5050)
 app = create_app("DevConfig")
-app.run(debug=True, host="0.0.0.0", port=int(DEV_PORT))
+app.run(debug=True, host="", port=int(DEV_PORT))
