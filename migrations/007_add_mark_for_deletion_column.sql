5e059b44bee299a91be11c1b72574ce3a046e26a
matthew-robinson-ons <matthew.robinson@ons.gov.uk>
2026-02-16 09:09:23 +0000

    tidy

--- migrations/007_add_mark_for_deletion_column.sql
+++ migrations/007_add_mark_for_deletion_column.sql
@@ -1,3 +1,3 @@
--- Migration to add mark_for_deletion as a nullable boolean column to secure_message table
+-- Migration to add mark_for_deletion as a nullable boolean column to conversation table
 ALTER TABLE securemessage.secure_message ADD COLUMN mark_for_deletion BOOLEAN;
 
